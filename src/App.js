40c1a78275d4f80b139908aae2b3879354b4a208
AndyPSI <andy@performance-software.com>
2019-09-19 11:59:53 -0500

    Config setup relocated to reducers index

--- src/App.js
+++ src/App.js
@@ -3,39 +3,9 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import Thunk from "redux-thunk";
-import _ from "lodash";
 import Main from "./components/Main";
-import reduxState from "./constants/config";
-import { combineReducers } from "redux";
-import { countContainer, testContainer } from "./reducers";
-
-const countContainerConfig = {
-  key: "root",
-  storage: storage,
-  // blackList: ["testContainer"],
-  migrate: state => {
-    return Promise.resolve(_.merge(reduxState, state));
-  }
-};
-
-// Can create a different store for these values,
-// Or can continue to not be persisted
-// Can wrap a PersistGate with a different store to utilize it in render
-const testContainerConfig = {
-  key: "root",
-  storage: storage,
-  blackList: ["countContainer"],
-  migrate: state => {
-    return Promise.resolve(_.merge(reduxState, state));
-  }
-};
-
-const rootReducer = combineReducers({
-  countContainer: persistReducer(countContainerConfig, countContainer),
-  testContainer: testContainer
-});
+import rootReducer from "./reducers";
 
 const store = createStore(rootReducer, {}, applyMiddleware(Thunk));
 const persistor = persistStore(store);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,24 @@
-import { combineReducers } from "redux";
 import countContainer from "./countContainer";
 import testContainer from "./testContainer";
 
-const reducers = {
-  countContainer,
-  testContainer
+import { combineReducers } from "redux";
+import reduxState from "../constants/config";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import _ from "lodash";
+
+const rootConfig = {
+  key: "root",
+  storage: storage,
+  migrate: state => {
+    return Promise.resolve(_.merge(reduxState, state));
+  }
 };
 
-export { countContainer, testContainer };
+const rootReducer = combineReducers({
+  countContainer: persistReducer(rootConfig, countContainer),
+  testContainer: testContainer
+});
+
+export default rootReducer;
